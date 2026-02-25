b419857df27eacfb9c8e7517f2c151783aa3dd29
hasil18 <hasildevmurari@gmail.com>
2026-02-25 12:57:16 +0530

    add map and mail

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,8 +2,9 @@ export default function Footer() {
   return (
     <footer className="bg-gray-900 text-gray-300 mt-20 py-8 text-center">
       <p>📍 Ahmedabad, Gujarat</p>
-      <p>📞 +91 9714810703</p>
-      <p>📧 hassildevmurari@gmail.com</p>
+      <p>📞 +91 9979206812</p>
+       <p>📞 +91 7874152686</p>
+      <p>📧 vskconstruction32@gmail.com</p>
       <p className="mt-4 text-sm">
         © {new Date().getFullYear()} VSK Construction
       </p>
